546dcb288b796a14db45ee415b2e5aad08ab0be2
Branlice <lishiwen@wclound.cn>
2022-10-31 17:56:40 +0800

    feat: prettier

---
+++ packages/prettier-config-qstaff/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "singleQuote": true,
+    "trailingComma": "all",
+    "printWidth": 100,
+    "tabWidth": 4,
+    "semi": true,
+    "bracketSpacing": true,
+    "arrowParens": "always",
+    "jsxBracketSameLine": false,
+    "jsxSingleQuote": false,
+    "quoteProps": "as-needed",
+    "requirePragma": false,
+    "insertPragma": false,
+    "proseWrap": "preserve",
+    "htmlWhitespaceSensitivity": "css",
+    "endOfLine": "lf"
+}
